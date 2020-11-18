c37162a08a06882b8144f9629b4a18c2a167b02a
Josh Gumerove <josh.gumerove@gmail.com>
2020-11-18 05:09:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
 family_members = ["Beth", "Barry", "Brennan", "Jeremy"]
-family_members.push("Liam")
+family_members << "Liam"
 end
 
 def add_element_to_start_of_array(array, element)
